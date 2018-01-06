6a23f644b309d7527d6182e1d8a4652f4a87081a
Jesus Vega <chuyspc@gmail.com>
2018-01-05 21:13:11 -0800

    Initialize React and ES6 enviroment

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log("hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    document.getElementById('mount')
+  );
+});
